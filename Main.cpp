460798421bb2e350f5cf763096f6ea2116559b80
aleja <alejandrovasquezesc@gmail.com>
2025-03-24 22:42:48 -0600

    Creating basic structure of the game

---
+++ Main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int choice;
+
+    do
+    {
+        cout << "\n=== 3d Treasure Game ===" << endl;
+        cout << "1. Start Game" << endl;
+        cout << "0. Exit" << endl;
+        cout << "Choose an option: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            break;
+        case 0:
+            cout << "\nExiting the game. Goodbye!" << endl;
+            break;
+        default:
+            cout << "\nInvalid option. Please try again." << endl;
+        }
+    } while (choice != 0);
+
+    return 0;
+}
---
+++ game/Game.h
@@ -0,0 +1,19 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+class Game
+{
+private:
+public:
+    void startGame()
+    {
+        cout << "hola";
+    }
+};
+
+#endif
